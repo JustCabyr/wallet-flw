1814226b3229a8e587f5807f8d82e84552a85410
justcabyr <asulaiman@dervac.com>
2021-11-17 23:55:33 +0100

    Fixed wallet operations management

--- app.js
+++ app.js
@@ -129,4 +129,84 @@ app.get("/response", async (req, res) => {
     console.log(response.data.data)
 });
 
+// Validating User wallet
+const validateUserWallet = async (userId) => {
+    try {
+      // check if user have a wallet, else create wallet
+      const userWallet = await Wallet.findOne({ userId });
+  
+      // If user wallet doesn't exist, create a new one
+      if (!userWallet) {
+        // create wallet
+        const wallet = await Wallet.create({
+          userId,
+        });
+        return wallet;
+      }
+      return userWallet;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  // Create Wallet Transaction
+  const createWalletTransaction = async (userId, status, currency, amount) => {
+    try {
+      // create wallet transaction
+      const walletTransaction = await WalletTransaction.create({
+        amount,
+        userId,
+        isInflow: true,
+        currency,
+        status,
+      });
+      return walletTransaction;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  // Create Transaction
+  const createTransaction = async (
+    userId,
+    id,
+    status,
+    currency,
+    amount,
+    customer
+  ) => {
+    try {
+      // create transaction
+      const transaction = await Transaction.create({
+        userId,
+        transactionId: id,
+        name: customer.name,
+        email: customer.email,
+        phone: customer.phone_number,
+        amount,
+        currency,
+        paymentStatus: status,
+        paymentGateway: "flutterwave",
+      });
+      return transaction;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  // Update wallet 
+  const updateWallet = async (userId, amount) => {
+    try {
+      // update wallet
+      const wallet = await Wallet.findOneAndUpdate(
+        { userId },
+        { $inc: { balance: amount } },
+        { new: true }
+      );
+      return wallet;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 module.exports = app;
